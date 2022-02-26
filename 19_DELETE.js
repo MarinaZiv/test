aa7a809f8ba432d4abab63d1ab2846fb28c9b5b6
Marina Ziv <93940538+MarinaZiv@users.noreply.github.com>
2022-02-26 21:52:58 +0200

    Update 19_DELETE.js

--- 19_DELETE.js
+++ 19_DELETE.js
@@ -49,9 +49,7 @@ app.get('/blogs/create', (req, res) => {
 
 // Creating a POST handler:
 app.post('/blogs', (req, res) => {
-
     const blog = new Blog(req.body);
-
     blog.save()
         .then((result) => {
             res.redirect('/blogs')
@@ -59,19 +57,13 @@ app.post('/blogs', (req, res) => {
         .catch((err) => {
             console.error(err);
         });
-
 });
 
 
 app.get('/blogs/:id', (req, res) => {
     const id = req.params.id;
-    //   console.log(id);
-    // now we need to retrive the document with this ID from the database
     Blog.findById(id)
-        .then((result) => {
-            // we want to render the ditails page 
-            // we want to pass through the data that we get back so the blog is what we call it but we can call the property anything we want 
-            // and that is going to ba equal to the result because this result will be the single blog based on this ID
+        .then((result) => {        
             res.render('details', { blog: result, title: 'Blog Details' })
         })
         .catch((err) => {
@@ -81,13 +73,7 @@ app.get('/blogs/:id', (req, res) => {
 });
 
 
-// Click on delete button sends a delete request to the server,
-// then we go out to the database from the server and delete the blog with that ID
-// we`re going to do that in vanilla JS on the front-end, so important to notice that
-// this is not gonna be JS that is running on the server.
-// this is going to be running in the browser and when we click on this.
-// because in our view, if we add a script tag at the bottom, any script inside the script tag is going
-// to run in the browser, not on the server!!
+
 
 app.delete('/blogs/:id', (req, res) => {
     const id = req.params.id;
@@ -95,16 +81,6 @@ app.delete('/blogs/:id', (req, res) => {
     Blog.findByIdAndDelete(id)
         // this goes out to the database, it finds a document by the ID and deletes it from the database
         .then((result) => {
-            // we going to send back some JSON to the browser.
-            // why are we not redirecting here? 
-            // when we send ajax request (which what this is because we`re doing it from JS) in node we cannot
-            // use a redirect as a response. We have to send maybe JSON or text data back to the browser.
-            // so what we`re gonna do is actually send some JSON data back to the browser and that JSON data 
-            // is gonna have a redirect property.
-            // when we receive that data back over here we`re gonna look at the redirect property that gonna be a URL to where we
-            // want to redirect to and that`s going to be from the browser because this is an ajax request
-
-            // this is a typical response we`d use for API where we can reach out with JS to get data or delete data.
             res.json({ redirect: '/blogs' })
         })
         .catch((err) => {
